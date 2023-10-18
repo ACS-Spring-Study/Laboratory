7448143129b5dfb988a0f6096248050f68f70333
NotableDeveloper <castlereal01@gmail.com>
2023-10-18 19:35:22 +0900

    test : PasswordValidator 테스트 클래스 작성
    - PasswordValidator에 대한 테스트 클래스 작성
    - 테스트 준비를 위한 setUp 메서드 작성
    - PasswordValidator 인스턴스가 Not null임을 증명하는 테스트 작성

---
+++ src/test/java/PasswordValidatorTest.java
@@ -0,0 +1,23 @@
+import PasswordStrength.PasswordStrength;
+import PasswordStrength.PasswordValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PasswordValidatorTest {
+    PasswordValidator pv;
+
+    @BeforeEach
+    @DisplayName("테스트 메서드가 실행되기 전에 반드시 호출되는 메서드")
+    void setUp(){
+        pv = new PasswordValidator("abcd");
+    }
+
+    @Test
+    @DisplayName("비밀번호 유효성 검사 객체가 Null이 아님을 증명하는 테스트")
+    void 유효성_검사_객체_Not_null_테스트(){
+        pv = new PasswordValidator("abcd");
+        Assertions.assertNotNull(pv);
+    }
+}
