cbb90c88c24984af1201a42c78719afbadfbe36f
sw801733 <sw801733@naver.com>
2023-10-20 10:49:32 +0900

    refact : 암호 검증 로직 메서드로 추출
    - 글자 개수 검증 checkLength 메서드로 추출
    - 0~9 숫자 포함 검증 containsDigit 메서드로 추출
    - 대문자 포함 검증 containsUpper 메시드로 추출

--- src/main/java/PasswordStrength/PasswordValidator.java
+++ src/main/java/PasswordStrength/PasswordValidator.java
@@ -15,6 +15,18 @@ public class PasswordValidator {
     this.password = password;
   }
 
+  private boolean checkLength() {
+    return password.length() >= 8;
+  }
+
+  private boolean containsDigit() {
+    return password.matches(".*[0-9].*");
+  }
+
+  private boolean containsUpper() {
+    return password.matches(".*[A-Z].*");
+  }
+
   public PasswordStrength check() {
 
     boolean lengthCase = false;
@@ -23,32 +35,18 @@ public class PasswordValidator {
     int caseCnt = 0;
 
     // 암호는 여덟 글자 이상이어야 한다.
-    int passwordLength = password.length();
 
-    if (passwordLength >= 8) {
-      lengthCase = true;
-      caseCnt++;
-    }
+    if (checkLength()) caseCnt++;
 
     // 암호는 0부터 9사이의 숫자를 포함한다.
-    if (password.matches(".*[0-9].*")){
-      numCase = true;
-      caseCnt++;
-    }
+    if (containsDigit()) caseCnt++;
 
     // 암호는 대문자를 포함한다.
-    if (password.matches(".*[A-Z].*")){
-      UpperCase = true;
-      caseCnt++;
-    }
+    if (containsUpper()) caseCnt++;
 
-    if (caseCnt == 1)
-        return PasswordStrength.WEAK;
-    else if (caseCnt == 2)
-        return PasswordStrength.NORMAL;
-    else if (caseCnt == 3)
-        return PasswordStrength.STRONG;
-    else
-      return PasswordStrength.WEAK;
+    if (caseCnt == 1) return PasswordStrength.WEAK;
+    if (caseCnt == 2) return PasswordStrength.NORMAL;
+    if (caseCnt == 3) return PasswordStrength.STRONG;
+    else return PasswordStrength.WEAK;
   }
 }
