706a97f65fe6ce625c307dfcc9f6b671e2436c46
sw801733 <sw801733@naver.com>
2023-10-20 09:57:37 +0900

    feat : 암호 강도 측정 기능 추가
    - 비밀번호의 길이로 여덟 글자 이상 검증
    - 정규 표현식으로 0~9 사이의 숫자 포함 여부 검증
    - 정규 표현식으로 대문자 포함 여부 검증

--- src/main/java/PasswordStrength/PasswordValidator.java
+++ src/main/java/PasswordStrength/PasswordValidator.java
@@ -16,6 +16,39 @@ public class PasswordValidator {
   }
 
   public PasswordStrength check() {
-    return PasswordStrength.WEAK;
+
+    boolean lengthCase = false;
+    boolean numCase = false;
+    boolean UpperCase = false;
+    int caseCnt = 0;
+
+    // 암호는 여덟 글자 이상이어야 한다.
+    int passwordLength = password.length();
+
+    if (passwordLength >= 8) {
+      lengthCase = true;
+      caseCnt++;
+    }
+
+    // 암호는 0부터 9사이의 숫자를 포함한다.
+    if (password.matches(".*[0-9].*")){
+      numCase = true;
+      caseCnt++;
+    }
+
+    // 암호는 대문자를 포함한다.
+    if (password.matches(".*[A-Z].*")){
+      UpperCase = true;
+      caseCnt++;
+    }
+
+    if (caseCnt == 1)
+        return PasswordStrength.WEAK;
+    else if (caseCnt == 2)
+        return PasswordStrength.NORMAL;
+    else if (caseCnt == 3)
+        return PasswordStrength.STRONG;
+    else
+      return PasswordStrength.WEAK;
   }
 }
