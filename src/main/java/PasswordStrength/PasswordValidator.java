402845816e9964e4f9617bcde4d2e46a7aa27a9d
hongjunpyo <ejhjjp11@naver.com>
2023-10-21 22:00:01 +0900

    feat : 코딩 컨벤션 적용 및 기능 구현
    - 구글 코딩 컨벤션 적용
    - 비밀번호 유효성 검사 구현

--- src/main/java/PasswordStrength/PasswordValidator.java
+++ src/main/java/PasswordStrength/PasswordValidator.java
@@ -16,6 +16,37 @@ public class PasswordValidator {
   }
 
   public PasswordStrength check() {
-    return PasswordStrength.WEAK;
+    // 만족 조건 횟수
+    int strengthCnt = 0;
+
+    // 문자열 길이
+    int PasswordLength = password.length();
+    // 숫자 포함 확인용 변수
+    int checkDigit = 0;
+    // 대문자 포함 확인용 변수
+    int checkUpper = 0;
+
+    // 문자열 길이 8이상인지 확인
+    if (PasswordLength >= 8) {
+      strengthCnt += 1;
+    }
+
+    // 문자열에 숫자가 포함되어있는지 확인
+    if (password.matches(".*[0-9].*")){
+      strengthCnt += 1;
+    }
+
+    // 문자열에 대문자가 포함되엉씨는지 확인
+    if (password.matches(".*[A-Z].*")) {
+      strengthCnt += 1;
+    }
+
+    if (strengthCnt == 3){
+      return PasswordStrength.STRONG; // 모든 조건 만족일 경우
+    } else if (strengthCnt == 2) {
+      return PasswordStrength.NORMAL; // 2개 조건 만족일 경우
+    } else {
+      return PasswordStrength.WEAK; // 1개 이하 조건 만족일 경우
+    }
   }
 }
