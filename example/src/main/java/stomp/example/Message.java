95a41da50259fd101e760b69f588d8ab473d8d19
NotableDeveloper <castlereal01@gmail.com>
2023-11-01 23:16:14 +0900

    feat : spring stomp project init
    Spring stomp 실습을 위한 브랜치 개설

---
+++ example/src/main/java/stomp/example/Message.java
@@ -0,0 +1,15 @@
+package stomp.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String channelId;
+    private String name;
+}
---
+++ example/src/main/java/stomp/example/MessageController.java
@@ -0,0 +1,21 @@
+package stomp.example;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageController {
+
+    // stomp 방식으로 메세지를 주고 받기 위해 의존성 주입
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+
+    // 메세지 발행 시 매핑 되는 주소 (클라이언트 측에서는 /pub/message 라는 주소 메세지를 보내면 된다.)
+    @MessageMapping("/message")
+    public void message(Message message) {
+        simpMessageSendingOperations.convertAndSend("/sub/" + message.getChannelId(), message); // 구독한 주소에게 메세지를 발행함
+    }
+}
