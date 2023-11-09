103a31ff0db5ad656ba0ace452f279726ae1ea09
sw801733 <sw801733@naver.com>
2023-11-09 22:22:56 +0900

    feat : ChatRoom 클래스 생성
    - 채팅방 모델을 위한 ChatRoom 클래스 생성

---
+++ src/main/java/stomp/example/ChatRoom.java
@@ -0,0 +1,21 @@
+package stomp.example;
+
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatRoom {
+
+    private String ownerName;
+    private String channelId;
+
+    public static ChatRoom roomCreate(String ownerName) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.ownerName = ownerName;
+        chatRoom.channelId = UUID.randomUUID().toString();
+        return chatRoom;
+    }
+
+}
