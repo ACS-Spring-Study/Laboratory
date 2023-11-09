b64aebaa1297e295aa63c2ad9e30a8c349f45caf
sw801733 <sw801733@naver.com>
2023-11-09 22:28:58 +0900

    feat : ChatRoomController 클래스 createRoom 메서드 추가
    - /chat/room POST 요청 처리를 위한 createRoom 메서드 추가

--- src/main/java/stomp/example/ChatRoomController.java
+++ src/main/java/stomp/example/ChatRoomController.java
@@ -1,10 +1,18 @@
 package stomp.example;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatRoomController {
 
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String ownerName) {
+        return ChatRoom.createRoom(ownerName);
+    }
 }
