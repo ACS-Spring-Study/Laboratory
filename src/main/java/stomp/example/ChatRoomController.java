8d0147653a2796619fdf223330f7ae3b97ef91ad
sw801733 <sw801733@naver.com>
2023-11-09 22:53:45 +0900

    fix : ChatRoomController 클래스 createRoom 메서드 매개변수 변경
    - body 를 통한 POST 처리를 위해 메서드 매개변수를 @RequestBody로 변경

--- src/main/java/stomp/example/ChatRoomController.java
+++ src/main/java/stomp/example/ChatRoomController.java
@@ -1,6 +1,7 @@
 package stomp.example;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +13,8 @@ public class ChatRoomController {
 
     @PostMapping("/room")
     @ResponseBody
-    public ChatRoom createRoom(@RequestParam String ownerName) {
+    public ChatRoom createRoom(@RequestBody ChatRoom reqBody) {
+        String ownerName = reqBody.getOwnerName();
         return ChatRoom.createRoom(ownerName);
     }
 }
