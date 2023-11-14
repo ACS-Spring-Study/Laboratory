f5985d138370b409adde466e7686bfb9071b10dc
NotableDeveloper <castlereal01@gmail.com>
2023-11-14 13:05:11 +0900

    feat : Book 클래스 작성
    - 서비스의 핵심 엔티티인 Book 클래스 작성
    - Book의 분류를 나타내는 BookCategory 클래스 작성
    - Book의 상태를 나타내는 BookStatus 클래스 작성

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+public class Book {
+
+  String title;
+  String isbn;
+  String author;
+  BookCategory category;
+  BookStatus status;
+}
---
+++ src/main/java/com/example/demo/entity/BookCategory.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public enum BookCategory {
+  HUMAN,
+  CS,
+  MATH
+}
