653a610cd7177cfb9b44d1e3f5b5f3044ea4f93f
nyk <minseol1496@google.com>
2023-12-10 14:10:49 +0900

    mariadb 연동 및 jdbcrepository 코드 작성

--- src/main/java/com/example/demo/repository/JdbcBookRepository.java
+++ src/main/java/com/example/demo/repository/JdbcBookRepository.java
@@ -1,11 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.entity.Book;
+import com.example.demo.domain.entity.BookCategory;
+import com.example.demo.domain.entity.BookStatus;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
@@ -16,6 +20,18 @@ public class JdbcBookRepository implements BookRepository {
   private static Connection connection;
   private static Statement statement;
   private static PreparedStatement preparedStatement;
+
+  private Book setBook(ResultSet resultSet) throws SQLException {
+    Book book = new Book();
+
+    book.setTitle(resultSet.getString("title"));
+    book.setIsbn(resultSet.getString("isbn"));
+    book.setAuthor(resultSet.getString("author"));
+    book.setCategory(BookCategory.valueOf(resultSet.getString("book_category")));
+    book.setStatus(BookStatus.valueOf(resultSet.getString("book_status")));
+
+    return book;
+  }
 
   public JdbcBookRepository(){
     try {
@@ -28,21 +44,113 @@ public class JdbcBookRepository implements BookRepository {
 
   @Override
   public Book save(Book book) {
+    String sql = "Insert into Book values(?, ?, ?, ?, ?)";
+
+    try {
+      preparedStatement = connection.prepareStatement(sql);
+
+      preparedStatement.setString(1, book.getTitle());
+      preparedStatement.setString(2, book.getIsbn());
+      preparedStatement.setString(3, book.getAuthor());
+      preparedStatement.setString(4, String.valueOf(book.getCategory()));
+      preparedStatement.setString(5, String.valueOf(book.getStatus()));
+
+      preparedStatement.executeUpdate();
+
+      System.out.println(book.getTitle() +" 책 등록 완료");
+
+      preparedStatement.close();
+
+      return book;
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
     return null;
+
   }
 
   @Override
   public Book findByISBN(String isbn) {
+
+    String sql = "SELECT * from Book where isbn = ?";
+
+    try {
+      preparedStatement = connection.prepareStatement(sql);
+
+      preparedStatement.setString(1, isbn);
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      if (resultSet.next()) {
+
+        Book book = setBook(resultSet);
+
+        System.out.println(book.getTitle() + " 책 조회 완료");
+
+        return book;
+
+      } else {
+        System.out.println("책을 찾을 수 없습니다.");
+      }
+
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
     return null;
+
   }
 
   @Override
   public boolean existsByIsbn(String isbn) {
-    return false;
+    String sql = "SELECT * from Book where isbn = ?";
+
+    try {
+      preparedStatement = connection.prepareStatement(sql);
+
+      preparedStatement.setString(1, isbn);
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      if (resultSet.next()) {
+        System.out.println(isbn + " 은 이미 존재하는 책입니다.");
+        return true;
+      } else {
+        System.out.println(isbn + "에 해당하는 책을 찾을 수 없습니다.");
+        return false;
+      }
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+      return false;
+    }
   }
 
   @Override
   public List<Book> findAll() {
+    List<Book> books = new ArrayList<>();
+
+    try {
+      String sql = "Select * from Book";
+      ResultSet resultset = statement.executeQuery(sql);
+
+      while (resultset.next()) {
+        Book book = setBook(resultset);
+
+        System.out.println("모든 책 조회 완료");
+
+        books.add(book);
+      }
+
+      resultset.close();
+      statement.close();
+
+      return books;
+
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
     return null;
   }
 }
