0b6c23dd46485e35997e868fa6611b4d5532d347
sw801733 <sw801733@naver.com>
2023-12-18 19:03:19 +0900

    fix : Book 엔티티 ID 에 Auto increment 적용

--- src/main/java/com/example/demo/domain/entity/Book.java
+++ src/main/java/com/example/demo/domain/entity/Book.java
@@ -2,6 +2,8 @@ package com.example.demo.domain.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,6 +22,7 @@ import lombok.Setter;
 public class Book {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   int id;
 
   @Column(name="title")
