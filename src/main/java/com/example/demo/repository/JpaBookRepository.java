53890711431921d6d86698f9c95c792fbc76a88a
ktj1999 <kimtj1999@naver.com>
2023-12-18 11:24:52 +0900

    fix : 속성 이름 따라 메서드 수정
    Book 클래스의 속성이름에 맞게 메서드명을 findByISBN에서 findByIsbn으로 수정

--- src/main/java/com/example/demo/repository/JpaBookRepository.java
+++ src/main/java/com/example/demo/repository/JpaBookRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaBookRepository extends JpaRepository<Book, Long> {
-
+	Book findByIsbn(String isbn);
 }
--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -72,7 +72,7 @@ public class BookService {
     BooksResponse response;
 
     try {
-      Book findBook = bookRepository.findByISBN(isbn);
+      Book findBook = bookRepository.findByIsbn(isbn);
 
       List<BookResponse> books = new ArrayList<>();
 
@@ -258,7 +258,7 @@ public class BookService {
     OrderResponse response;
 
     try {
-      Book book = bookRepository.findByISBN(borrowBookDTO.getIsbn());
+      Book book = bookRepository.findByIsbn(borrowBookDTO.getIsbn());
 
       if (book.getStatus() == BookStatus.AVAILABLE) {
         book.setStatus(BookStatus.BORROWING);
@@ -287,7 +287,7 @@ public class BookService {
     OrderResponse response;
 
     try {
-      Book book = bookRepository.findByISBN(returnBookDTO.getIsbn());
+      Book book = bookRepository.findByIsbn(returnBookDTO.getIsbn());
 
       if (book.getStatus() == BookStatus.AVAILABLE) {
         throw new RuntimeException("Book is available!");
