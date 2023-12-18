eef6eba2ea0494299ad1d6356be6cabde30ff0b8
hongjunpyo <ejhjjp11@naver.com>
2023-12-18 19:21:02 +0900

    feat : Book 클래스 수정 - Entity 어노테이션 추가 - 기본키를 사용하는 id 값 추가

--- src/main/java/com/example/demo/domain/entity/Book.java
+++ src/main/java/com/example/demo/domain/entity/Book.java
@@ -1,17 +1,26 @@
 package com.example.demo.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
   String title;
   String isbn;
   String author;
--- src/main/java/com/example/demo/repository/JpaBookRepository.java
+++ src/main/java/com/example/demo/repository/JpaBookRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.entity.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaBookRepository extends JpaRepository<Book, String> {
-  
+  List<Book> findByISBN(String isbn);
 }
