807d328a908902122dd2fdb1792c8b1aebc20a8b
sw801733 <sw801733@naver.com>
2023-12-18 17:35:17 +0900

    feat : JpaBookRepository title 검색을 위한 메서드 추가

--- src/main/java/com/example/demo/repository/JpaBookRepository.java
+++ src/main/java/com/example/demo/repository/JpaBookRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.entity.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaBookRepository extends JpaRepository<Book, String> {
     Book findByIsbn(String isbn);
+    List<Book> findByTitleContaining(String title);
 }
