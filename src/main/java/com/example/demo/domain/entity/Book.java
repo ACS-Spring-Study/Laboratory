45f9a15fa849172097d85ff0519cdd22e1d28c00
sw801733 <sw801733@naver.com>
2023-12-18 11:46:32 +0900

    feat : Book 클래스에 엔티티 매핑을 위한 어노테이션 추가

--- src/main/java/com/example/demo/domain/entity/Book.java
+++ src/main/java/com/example/demo/domain/entity/Book.java
@@ -1,11 +1,17 @@
 package com.example.demo.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name="book")
 @Getter
 @Setter
 @Builder
@@ -13,9 +19,21 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Book {
 
+  @Id
+  int id;
+
+  @Column(name="title")
   String title;
+
+  @Column(name="isbn")
   String isbn;
+
+  @Column(name="author")
   String author;
+
+  @Column(name="book_category")
   BookCategory category;
+
+  @Column(name="book_status")
   BookStatus status;
 }
