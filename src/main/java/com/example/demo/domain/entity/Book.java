85797af2872ea2b671049f8594717c769b977bab
ktj1999 <kimtj1999@naver.com>
2023-12-18 21:26:29 +0900

    feat : 필드와 컬럼 매핑
    객체 필드를 테이블 컬럼에 매핑시키기 위한 @Column 추가

--- src/main/java/com/example/demo/domain/entity/Book.java
+++ src/main/java/com/example/demo/domain/entity/Book.java
@@ -6,11 +6,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 
 @Getter
 @Setter
@@ -23,11 +26,20 @@ public class Book {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(name="title")
   String title;
+
+  @Column(name="isbn")
   String isbn;
+
+  @Column(name="author")
   String author;
+
   @Enumerated(EnumType.STRING)
+  @Column(name="book_category")
   BookCategory category;
+
   @Enumerated(EnumType.STRING)
+  @Column(name="book_status")
   BookStatus status;
 }
